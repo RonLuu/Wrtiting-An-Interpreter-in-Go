9d59064243febfbf14ebc5750e07e1b64a0d7260
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-07-20 08:42:29 +1000

    Add a REPL

---
+++ Chapter_1/repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"Chapter_1/lexer"
+	"Chapter_1/token"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
