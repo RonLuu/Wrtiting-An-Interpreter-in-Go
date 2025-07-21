444c0f5913fafb66e486bee18597afff5d71c102
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-07-21 14:57:22 +1000

    Add a test for return statement

--- Chapter_2/parser/parser_test.go
+++ Chapter_2/parser/parser_test.go
@@ -22,7 +22,7 @@ let foobar = 838383;
 	}
 
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+		t.Fatalf("program.Statements does not contain 3 statements. got = %d", len(program.Statements))
 	}
 
 	tests := []struct {
@@ -39,21 +39,6 @@ let foobar = 838383;
 			return
 		}
 	}
-}
-
-func checkParserErrors(t *testing.T, p *Parser) {
-	errors := p.Errors()
-	if len(errors) == 0 {
-		return
-	}
-
-	t.Errorf("parser has %d errors\n", len(errors))
-
-	for _, msg := range errors {
-		t.Errorf("Parser error: %q", msg)
-	}
-
-	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, stmt ast.Statement, expectedVariable string) bool {
@@ -79,4 +64,56 @@ func testLetStatement(t *testing.T, stmt ast.Statement, expectedVariable string)
 	}
 
 	return true
+}
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got = %d", len(program.Statements))
+	}
+
+	// tests := []struct {
+	// 	expectedVariable string
+	// }{
+	// 	{"x"},
+	// 	{"y"},
+	// 	{"foobar"},
+	// }
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got = %T", returnStmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got = %q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors\n", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("Parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
